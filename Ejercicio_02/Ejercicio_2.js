23e6ef2b153de65de0ed04ff60a0a90aa8dad9e9
EdwinSantiago1 <FAMILIA RIAO OROZCO@DESKTOP-CUT75OH>
2022-09-22 15:26:32 -0500

    Ejercicio 2

---
+++ Ejercicio_02/Ejercicio_2.js
@@ -0,0 +1,15 @@
+// libro favorito
+const libro ={
+    titulo : "Cien años de soledad",
+    autor : "Gabriel Garcia Marquez",
+    fecha : "1967",
+    url : "http://www.secst.cl/upfiles/documentos/19072016_1207am_578dc39115fe9.pdf"
+}
+
+// Mi fecha de nacimiento
+const nacimiento = new Date("march 16 2001");
+
+// Lista
+const yo = ["Santiago", 21, false, nacimiento, libro];
+
+console.log(yo);
